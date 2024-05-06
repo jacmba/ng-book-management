a6ad55aec350b7d3e00df9c50ae4a0671f39e014
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-06 20:44:10 +0100

    Reactive implementation

---
+++ src/app/books/book.effects.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { BookService } from "./book.service";
+import { catchError, map, mergeMap, of } from "rxjs";
+import * as bookActions from './book.actions';
+
+@Injectable()
+export class BookEffects {
+
+  addBook$ = createEffect(() => this.actions$.pipe(
+    ofType(bookActions.AddBook),
+    mergeMap((action) => this.bookService.addBook(action)
+      .pipe(
+        map(book => bookActions.AddBookSuccess(book)),
+        catchError(error => of(bookActions.AddBookFailure({error})))
+      ))
+    )
+  );
+
+  constructor (private actions$: Actions,
+    private bookService: BookService
+  ) {}
+}
---
+++ src/app/books/book.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from "@ngrx/store";
+import { Book } from "../models/book";
+import { AddBook, AddBookFailure, AddBookSuccess, RemoveBook } from "./book.actions";
+
+export const initialState: Book[] = [];
+
+export const BookReducer = createReducer(
+  initialState,
+  on(AddBook, (state) => state),
+  on(AddBookSuccess, (state, {id, title, author}) => [...state, {id, title, author}]),
+  on(AddBookFailure, (state, {error}) => {
+    console.error(error);
+    return state;
+  }),
+  on(RemoveBook, (state, {bookId}) => state.filter(b => b.id !== bookId))
+);
